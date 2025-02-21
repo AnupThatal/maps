977d4f9dbbdaf3cb9316c699455a8f9aaa8c003d
Anup Thatal <53684908+AnupThatal@users.noreply.github.com>
2025-02-21 13:17:43 +0545

    Update home.py

--- home.py
+++ home.py
@@ -1,113 +1,109 @@
 import streamlit as st
 import pandas as pd
-import plotly.express as px
+import folium
+from folium import plugins
+from streamlit_folium import st_folium
 import requests
-import time
-import configparser
-import os
 
 
-start = time.time()
 
-@st.cache
-def fetch_and_process_data(sdma):
+def data_collection():
     odata_urls = [
-        'https://survey.kuklpid.gov.np/v1/projects/7/forms/kukl_customer_survey_01.svc',
-        'https://survey.kuklpid.gov.np/v1/projects/7/forms/kukl_customer_survey.svc',
-        'https://survey.kuklpid.gov.np/v1/projects/15/forms/kukl_customer_survey_01.svc',
-        'https://survey.kuklpid.gov.np/v1/projects/9/forms/kukl_customer_survey_01.svc',
-        'https://survey.kuklpid.gov.np/v1/projects/6/forms/kukl_customer_survey_01.svc',
-        'https://survey.kuklpid.gov.np/v1/projects/2/forms/kukl_customer_survey_01.svc',
-        'https://survey.kuklpid.gov.np/v1/projects/2/forms/kukl_customer_survey.svc',
-        'https://survey.kuklpid.gov.np/v1/projects/16/forms/kukl_customer_survey_01.svc'
+        'https://survey.kuklpid.gov.np/v1/projects/20/forms/kukl_customer_survey_phase1.svc'
     ]
-
-    headers = {
-        'Accept': 'application/json'}  
-    
-    params = {
-        '$select': 'unique_form_id,b10_sub_dmi,gb12_skip/gc01_skp1/gc20/c20,gb12_skip/gc01_skp1/gc20/c22,__system/submitterName,__system/reviewState,b02,unit_owners,gb12_skip/gc01_skp2/d08',
-    }
-    
     submission_entity_set = 'Submissions'
     username = 'anupthatal2@gmail.com'
     password = 'Super@8848'
     session = requests.Session()
     session.auth = (username, password)
-
-    all_dfs = []
+    
+    all_dfs = []  # List to store DataFrames from each URL
+    
     for odata_url in odata_urls:
         submission_url = f"{odata_url}/{submission_entity_set}"
-        try:
-            response = session.get(submission_url, params=params, headers=headers)
-            response.raise_for_status()
-
+        response = session.get(submission_url)
+        if response.status_code == 200:
             data = response.json()
-
-            if 'value' in data:
-                df = pd.DataFrame(data['value'])
-
-                if 'b10_sub_dmi' in df.columns:
-                    if not df.empty:
-                        all_dfs.append(df)
-
-        except requests.exceptions.RequestException as e:
-            continue 
-
-    if not all_dfs:
-        st.warning("No data retrieved for the specified location.")
-        return pd.DataFrame()
-
+            df = pd.DataFrame(data['value'])
+            pd.set_option('display.max_columns', None)
+            print(df)
+            df['lat'] = df['b02'].apply(lambda x: x['coordinates'][1] if isinstance(x, dict) and 'coordinates' in x else None)
+            df['lon'] = df['b02'].apply(lambda x: x['coordinates'][0] if isinstance(x, dict) and 'coordinates' in x else None)
+            customer = []
+            connection = []
+            submittername = []
+            reviewState = []
+            for i in df['gb12_skip']:
+                customer.append(i['gc01_skp1']['gc20']['c20'])
+                connection.append(i['gc01_skp1']['gc20']['c22'])
+            for i in df['__system']:
+                submittername.append(i['submitterName'])
+                reviewState.append(i['reviewState'])
+            # for i in df['gb10-b10_package']:
+            #     packages.append(i['b10_dni']['b10_dmi'])
+            #     print(packages)
+            df['ReviewState'] = reviewState
+            df['SubmitterName'] = submittername
+            df['gb12_skip-gc01_skp1-gc20-c20'] = customer
+            df['gb12_skip-gc01_skp1-gc20-c22'] = connection
+            df['SubmitterName'] = df['SubmitterName'].str.upper()
+            df = df[['b10_dmi','ward_number','unique_form_id','lat','lon','gb12_skip-gc01_skp1-gc20-c20', 'gb12_skip-gc01_skp1-gc20-c22','SubmitterName', 'ReviewState', 'unit_owners']]
+            all_dfs.append(df)  # Append the processed DataFrame
     final_df = pd.concat(all_dfs, ignore_index=True)
-    final_df['gb12_skip-gc01_skp1-gc20-c20'] = final_df['gb12_skip'].apply(lambda x: x.get('gc01_skp1', {}).get('gc20', {}).get('c20'))
-    final_df['gb12_skip-gc01_skp1-gc20-c22'] = final_df['gb12_skip'].apply(lambda x: x.get('gc01_skp1', {}).get('gc20', {}).get('c22'))
-    final_df['SubmitterName'] = final_df['__system'].apply(lambda x: x['submitterName'] if 'submitterName' in x else None).str.upper()
-    final_df['ReviewState'] = final_df['__system'].apply(lambda x: x['reviewState'] if 'reviewState' in x else None)
-    final_df['b02-Longitude'] = final_df['b02'].apply(lambda x: x['coordinates'][0] if (x and 'coordinates' in x) else None)
-    final_df['b02-Latitude'] = final_df['b02'].apply(lambda x: x['coordinates'][1] if (x and 'coordinates' in x) else None)
-    final_df['gb12_skip-gc01_skp2-d08'] = final_df['gb12_skip'].apply(lambda x: x.get('gc01_skp2', {}).get('d08'))
-
     return final_df
 
-st.set_page_config(page_title="Map Display App", page_icon=":earth_americas:")
-st.title("Data Fetch and Map Display App")
-location_area = st.text_input('Select location')
-total_df=pd.read_csv('HHC_Data.csv')
-total_df['sDMA'] = total_df['sDMA'].str.replace('.','')
-total_df['SDMA wise HHC'] = total_df['SDMA wise HHC'].str.replace(',', '')
-total_df['SDMA wise HHC'] = total_df['SDMA wise HHC'].str.replace(' ','')
-total = total_df[total_df['sDMA'] == location_area]
-if location_area:
-    try:
-        result_df = fetch_and_process_data(sdma=location_area)
-        
-    except Exception as e:
-        st.warning("An error occurred while fetching data. Please try again later.")
-else:
-    result_df = pd.DataFrame()
+df = data_collection()
 
-# Display map or warning
-if not result_df.empty:
-        st.success(f"Data successfully retrieved and processed of sub_dma: {location_area}")
-        selected_df = result_df[result_df['b10_sub_dmi'] == location_area].dropna(subset=['b02-Latitude', 'b02-Longitude'])
-        num=selected_df['unique_form_id'].nunique()
-        st.caption(f"Data of collection of that areas: {num}")
-        tc=total['SDMA wise HHC'].values[0]
-        areas=total['Areas'].values[0]
-        ward=total['ward number'].values[0]
-        st.caption(f"Total data of that areas: {tc}")
-        st.caption(f"Areas : {areas}")
-        st.caption(f"Ward : {ward}")
-        if not selected_df.empty:
-            fig = px.scatter_mapbox(selected_df, 
-                                    lat='b02-Latitude', 
-                                    lon='b02-Longitude', 
-                                    hover_name='SubmitterName', 
-                                    zoom=15,
-                                    height=800)
 
-            fig.update_layout(mapbox_style="open-street-map")
-            st.plotly_chart(fig)
-            end=time.time()
-            diff=start-end
-            st.write(f'details of time taken {diff}')
+# Sidebar
+with st.sidebar:
+    
+    location=df['ward_number'].unique().tolist()
+    # location_area=st.selectbox('Select location',location)
+    areas_list = df['b10_dmi'].dropna().unique().tolist()
+    selected_area = st.selectbox('Select Area', areas_list)
+    if selected_area:
+        filtered_df = df[df['b10_dmi'] == selected_area]
+        # filtered_df=df1[df1['Areas']==location_area]
+        # P=filtered_df['Packages']
+        SDMA=filtered_df['b10_dmi'].unique().tolist()[0]
+        ward=filtered_df['ward_number'].unique().tolist()[0]
+    
+        # person=filtered_df['Person']
+        # phone=filtered_df['Phone']
+        sub_dmi_counts = filtered_df['b10_dmi'].value_counts()
+        st.write(f"ward of that areas :blue[{ward}]")
+        # st.write(f'Packages of :blue[{packages}]')
+        st.write(f"ward of :blue[{SDMA}]")
+        # st.write(f'Person responsible :blue[{person}]')
+        # st.write(f'Phone number of that person :blue[{phone}]')
+        st.write(sub_dmi_counts)
+
+# Main content
+col1, = st.columns(1)  # Note the use of comma to unpack the list
+
+with col1:
+    if selected_area:
+        # Filter DataFrame for selected area
+        selected_df = df[df['b10_dmi'] == selected_area]
+
+        # Drop rows with NaN values in location coordinates
+        selected_df = selected_df.dropna(subset=['lat', 'lon'])
+
+        # Extract latitude and longitude lists
+        lat = selected_df['lat'].astype(float).tolist()
+        lon = selected_df['lon'].astype(float).tolist()
+
+        # Calculate the center of the map
+        center_lat = sum(lat) / len(lat) if len(lat) > 0 else 0
+        center_lon = sum(lon) / len(lon) if len(lon) > 0 else 0
+
+        # Create a Folium map centered at the mean of coordinates
+        folium_map = folium.Map(location=[center_lat, center_lon], zoom_start=16)
+
+        # Add markers for each location with smaller icon
+        for i in range(len(lat)):
+            folium.Marker([lat[i], lon[i]], icon=folium.Icon(icon="circle", prefix='fa', icon_color='blue', icon_size=(2,2))).add_to(folium_map)
+
+        # Display the Folium map using streamlit_folium
+        st_folium(folium_map,width=1000, height=500)
